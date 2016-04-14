467dc25fe8a99c5142dd27470e53982b8f0951ae
John Hwang <johnsies87@gmail.com>
2016-04-14 16:22:24 -0700

    assessment static status bar

--- scripts.js
+++ scripts.js
@@ -1,19 +1,45 @@
 
+// $(function() {
+//     var $pokemons = $('#table tr:last');
+//     $.ajax({
+//         type: 'GET',
+//         url: 'http://pokeapi.co/api/v2/pokemon/',
+//         success: function(pokemons) {
+//             console.log(pokemons)
+//             $.each(pokemons.results, function(i, pokemon){
+//                 console.log(pokemon)
+//                 $pokemons.after('<tr class="table-content"><td></td><td>' +pokemon.name +'</td><td>' +pokemon.url +'</td><td>heart, sports</td><tr>');
+//             })
+//         }
+//     })
+// });
+
+
+// search for clients
 $(function() {
-    var $pokemons = $('#table tr:last');
+    var $clients = $('#table tr:last');
     $.ajax({
         type: 'GET',
-        url: 'http://pokeapi.co/api/v2/pokemon/',
-        success: function(pokemons) {
-            console.log(pokemons)
-            $.each(pokemons.results, function(i, pokemon){
-                console.log(pokemon)
-                $pokemons.after('<tr class="table-content"><td></td><td>' +pokemon.name +'</td><td>' +pokemon.url +'</td><td>heart, sports</td><tr>');
+        url: 'http://registry-api-staging.optimispt.com/clients/search?q=Umang',
+        success: function(clients) {
+            console.log(clients)
+            $.each(clients, function(i, client){
+                console.log(client)
+                $clients.after('<tr class="table-content"><td></td><td>' +client.first_name +'</td><td>' + client.last_name +'</td><td>heart, sports</td><tr>');
             })
         }
     })
 });
 
+$(function(){
+    console.log('ready');
 
+    $('#submit').click(function(){
+        var search_term = {
+            q: 'umang'
+        };
+        console.dir(search_term)
+    });
+});
 
 
